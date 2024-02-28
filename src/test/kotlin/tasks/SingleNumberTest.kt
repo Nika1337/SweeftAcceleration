00336e0afd59624ca0defa0f74744914ccf49bb9
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2024-02-28 17:52:14 +0400

    added implementation for SingleNumber and tested it using SingleNumberTest.kt

---
+++ src/test/kotlin/tasks/SingleNumberTest.kt
@@ -0,0 +1,30 @@
+package tasks
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+
+class SingleNumberTest {
+
+    private lateinit var singleNumber: SingleNumber
+    @BeforeEach
+    fun initialize() {
+        singleNumber = SingleNumber()
+    }
+    @Test
+    fun singleNumber_invokeOnEmptyArray_returnMinusOne() {
+        val testArray = intArrayOf()
+        val expected = -1
+        val actual = singleNumber(testArray)
+        assertEquals(expected, actual)
+    }
+    @Test
+    fun singleNumber_invokeOnCorrectArray_returnsOddOneOut() {
+        val testArray = intArrayOf(2,2,1,3,3)
+        val expected = 1
+        val actual = singleNumber(testArray)
+        assertEquals(expected, actual)
+    }
+
+}
