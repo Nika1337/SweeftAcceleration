77b0cd869d002bbd68fa75731fa449735689ffe1
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2024-02-28 23:56:32 +0400

    moved SingleNumberTest to its respective folder

--- src/test/kotlin/tasks/singlenumber/SingleNumberTest.kt
+++ src/test/kotlin/tasks/singlenumber/SingleNumberTest.kt
@@ -1,10 +1,9 @@
-package tasks
+package tasks.singlenumber
 
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
-import tasks.singlenumber.SingleNumber
 
 class SingleNumberTest {
 
