1626c358e776861f1409ac441bd436bb74e185ec
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2024-02-29 22:55:25 +0400

    added basic structure for DataStructure, but need to implement it

---
+++ src/main/kotlin/tasks/constanttimedeletedatastructure/ConstantTimeDeleteDataStructure.kt
@@ -0,0 +1,18 @@
+package tasks.constanttimedeletedatastructure
+
+class ConstantTimeDeleteDataStructure<K, V> : DataStructure<K, V> {
+
+    override fun insert(key: K, value: V) {
+        TODO("Not yet implemented")
+    }
+
+    override fun get(key: K): V {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(key: K): V {
+        TODO("Not yet implemented")
+    }
+
+
+}
---
+++ src/main/kotlin/tasks/constanttimedeletedatastructure/DataStructure.kt
@@ -0,0 +1,7 @@
+package tasks.constanttimedeletedatastructure
+
+interface DataStructure<K,V> {
+    fun insert(key: K, value: V)
+    fun get(key: K): V
+    fun delete(key: K): V
+}
